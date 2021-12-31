8a1af9fd9b227646605417b761d2b9a934eb0a18
PadreAlex <67380935+PadreAlex@users.noreply.github.com>
2021-12-31 16:39:54 +0300

    Update sketch.js

--- Random_In_circle/sketch.js
+++ Random_In_circle/sketch.js
@@ -1,20 +1,29 @@
+var points = []
+var multipl = 0.008
+var density = 40
 
-// let gap = 10
-// let circNum = 40
-// let circSize = 20
-let angle
 var r1 
 var r2
 var g1
 var g2
 var b1
 var b2
-var arr = []
+var nDet = 1
+
 function setup() {
-
-  createCanvas(700, 500);
+  createCanvas(720, 550);
+  background(0);
   angleMode(DEGREES)
+  noiseDetail(nDet)
 
+  var space = width / density
+
+  for (let x = 0; x < width; x += space) {
+    for (let y = 0; y < height; y += space) {
+      var vector = createVector(x + random(-50, 50), y + random(-50, 50))
+      points.push(vector)
+    }
+  }
 
   r1 = random(255)
   r2 = random(255)
@@ -22,68 +31,28 @@ function setup() {
   g2 = random(255)
   b1 = random(255)
   b2 = random(255)
-  
+  multipl = random(0.001, 0.01)
+  density = random(10, 100)
+  nDet = random(1, 100)
 }
 
-function draw() {  
-  background(0);
-  drawFirst()
-  noLoop()
-}
-function drawFirst() {
-  let gap = random(7, 20)
-  let circNum = random(30, 50)
-  let circSize = 30
-  push()
-  translate(width / 2 + 100, height / 2 - 40)
-  angle = map(random(height), 0, random(width), 0, -360, 360)
-  rotate(angle)
-  noFill()
-  strokeWeight(1)
-  for (let i = 0; i < circNum; i++) {
-    let r = map(circSize + gap * i, 0, width, r1, r2)
-    let g = map(circSize + gap * i, 0, height, g1, g2)
-    let b = map(circSize + gap * i, 0, width, b1, b2)
-    // var alpha = map(circSize + gap * i - 300, 0, 255, 255, 50)
-    stroke(r, g, b, random(50, 255))
-    arc(0, 0, circSize + gap * i, circSize + gap * i, angle * random(3000), 360 - random(2000) )
+function draw() {
+  noStroke()
+
+
+  for (let i = 0; i < points.length; i++) {
+    var r = map(points[i].x, 0, width, r1, r2)
+    var g = map(points[i].y, 0, height, g1, g2)
+    var b = map(points[i].x, 0, width, b1, b2)
+    var alpha = map(dist(width / 2, height / 2, points[i].x, points[i].y), 0, 250, 250, 0)
+    fill(r, g, b, alpha)
+
+    var angle = map(noise(points[i].x * multipl, points[i].y * multipl),     0 , 1, 0, 720)
+
+    points[i].add(createVector(cos(angle), sin(angle)))
+
+    if (dist(width / 2, height / 2, points[i].x, points[i].y) < 250) {
+       ellipse(points[i].x, points[i].y, 1) 
+    }
   }
-  pop()
-}
-
-// function drawSecond() {
-//    let gap = random(7, 10)
-//   let circNum = random(20, 40)
-//   let circSize = random(10, 20)
-//   r1 = random(255)
-//   r2 = random(255)
-//   g1 = random(255)
-//   g2 = random(255)
-//   b1 = random(255)
-//   b2 = random(255)
-//   push()
-//   translate(width / 2 - 100, height / 2 + 40)
-//   angle = map(random(height), 0, random(width), 0, -360, 360)
-//   rotate(angle)
-//   noFill()
-//   strokeWeight(1)
-//   for (let i = 0; i < circNum; i++) {
-//     let r = map(circSize + gap * i, 0, width, r1, r2)
-//     let g = map(circSize + gap * i, 0, height, g1, g2)
-//     let b = map(circSize + gap * i, 0, width, b1, b2)
-//     stroke(r, g, b)
-//     arc(0, 0, circSize + gap * i, circSize + gap * i, angle * random(3000), 360 - random(2000) )
-//   }
-
-//   pop()
-//   noLoop()
-// }
-function exportHighResolution() {
-  saveCanvas('myCanvas', 'jpg')
-}
-function keyReleased() { if (key == 'e') exportHighResolution(); }
-
-function mousePressed() {
-  // clear();
-  redraw(20);
 }
